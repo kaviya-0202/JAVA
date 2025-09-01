2f0df963d17b9d798a6095fce5d8d131f3e4224c
Kaviya.S <kaviyakaviya11376@gmail.com>
2025-09-01 20:42:16 +0530

    Add files via upload

---
+++ pat6.java
@@ -0,0 +1,23 @@
+public class pat6 {
+    public static void main(String[] args) {
+        int n = 5; 
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
